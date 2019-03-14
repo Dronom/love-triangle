6363cce6a2f88e36780eed563ae9de06980034a0
Roman <dronymys@mail.ru>
2019-03-14 18:28:48 +0300

    feat:make a love-triangle

--- src/index.js
+++ src/index.js
@@ -2,6 +2,17 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
+//let preferences = [3,1,2,5,6,4];
+//let number = getLoveTrianglesCount(preferences);
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let counter=0;
+  for(let i=0;i<preferences.length;i++){
+    let j=preferences[i];
+    j=preferences[j-1];
+    j=preferences[j-1];
+    if(i == (j-1)){counter++;}
+  }
+  console.log(counter);
+  return Math.floor(counter/3);
 };
+
