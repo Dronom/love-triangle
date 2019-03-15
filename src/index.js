0cd666fd83bd6685130de7b3199c0dd271b3b0e6
Roman <dronymys@mail.ru>
2019-03-15 12:44:00 +0300

    feat:make a check for  equal numbers

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-//let preferences = [3,1,2,5,6,4];
-//let number = getLoveTrianglesCount(preferences);
 module.exports = function getLoveTrianglesCount(preferences = []) {
   let counter=0;
   for(let i=0;i<preferences.length;i++){
     let j=preferences[i];
+    if(j == preferences[j-1]) continue;
     j=preferences[j-1];
     j=preferences[j-1];
     if(i == (j-1)){counter++;}
   }
-  console.log(counter);
-  return Math.floor(counter/3);
+  return (counter/3);
 };
 
